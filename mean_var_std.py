9475c00d01c8f254bf25ee1d79d6c270726d492a
Thiago Cabre Crawford <thiagocabre@gmail.com>
2025-03-20 01:13:21 +0000

    Completed project

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,37 @@
 import numpy as np
 
-def calculate(list):
+def calculate(l: list):
 
+    if len(l) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    # Convert list into matrix
+    M = np.array(l).reshape(3, 3)
+    
+    # Create empty lists to store values
+    calculations = {
+        'mean': [],
+        'variance': [],
+        'standard deviation': [],
+        'max': [],
+        'min': [],
+        'sum': []
+    }
 
-
+    for i in range(3):
+        if i == 2:
+            calculations['mean'].append(M.mean().item())
+            calculations['variance'].append(M.var().item())
+            calculations['standard deviation'].append(M.std().item())
+            calculations['max'].append(M.max().item())
+            calculations['min'].append(M.min().item())
+            calculations['sum'].append(M.sum().item())
+        else:
+            calculations['mean'].append(M.mean(axis = i).tolist())
+            calculations['variance'].append(M.var(axis = i).tolist())
+            calculations['standard deviation'].append(M.std(axis = i).tolist())
+            calculations['max'].append(M.max(axis = i).tolist())
+            calculations['min'].append(M.min(axis = i).tolist())
+            calculations['sum'].append(M.sum(axis = i).tolist())
 
     return calculations
